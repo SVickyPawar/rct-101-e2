c132f5038504456201994dbebcc5c878fad8ef82
SVickyPawar <97332040+SVickyPawar@users.noreply.github.com>
2022-05-30 16:52:20 +0530

    code initiated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { Text,Image,Box,Stack,Heading,Tag,TagLabel } from '@chakra-ui/react'
 
-const Product = () => {
+const Product = ({key,title,src,gender,price,category}) => {
   // TODO: Remove below const and instead import them from chakra
-  const Text = () => <div />;
-  const Image = () => <div />;
-  const Box = () => <div />;
-  const Stack = () => <div />;
-  const Heading = () => <div />;
-  const Tag = () => <div />;
-  const TagLabel = () => <div />;
+  // const Text = () => <div />;
+  // const Image = () => <div />;
+  // const Box = () => <div />;
+  // const Stack = () => <div />;
+  // const Heading = () => <div />;
+  // const Tag = () => <div />;
+  // const TagLabel = () => <div />;
   return (
-    <Stack data-cy="product">
-      <Image data-cy="product-image" />
-      <Text data-cy="product-category"></Text>
+    <Stack data-cy="product" >
+      <Image data-cy="product-image" src={src} />
+      <Text data-cy="product-category">{category}</Text>
       <Tag>
-        <TagLabel data-cy="product-gender"></TagLabel>
+        <TagLabel data-cy="product-gender">{gender}</TagLabel>
       </Tag>
-      <Heading data-cy="product-title"></Heading>
-      <Box data-cy="product-price"></Box>
+      <Heading data-cy="product-title">{title}</Heading>
+      <Box data-cy="product-price">{price}</Box>
     </Stack>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,50 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import AddProduct from "./AddProduct";
+import Pagination from "./Pagination";
+import Product from "./Product";
+import { Grid, GridItem } from '@chakra-ui/react'
+import { Flex, Spacer } from '@chakra-ui/react'
+import axios from "axios";
+
 
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  // const Flex = () => <div />;
+  // const Grid = () => <div />;
+   
+  const [list,setList]=useState([]);
+
+    useEffect(()=>{
+      getData();
+    },[]);
+
+    const getData=async()=>{
+      let res=await axios.get("http://localhost:8080/products");
+      let data=res.data;
+      setList(data)
+    }
+
+  // useEffect(()=>{
+  //   fetch("http://localhost:8080/products")
+  //   .then((r)=>r.json)
+  //   .then((d)=>{
+  //     setList(d)
+  //   })
+    
+  // },[])
+  
 
   return (
     <Flex>
       {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
+      <AddProduct/>
+      <Grid>{/* List of Products */}
+      {list.map((e)=>{
+        <Product key={e.id} src={e.imageSrc} title={e.title} category={e.category} gender={e.gender} price={e.price}/>
+      })}
+      </Grid>
       {/* Pagination */}
+      <Pagination/>
     </Flex>
   );
 };
